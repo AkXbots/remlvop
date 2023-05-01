73de2a0beea9e0f08938f61334d53e45e953f1b1
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-05-01 12:19:39 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -134,7 +134,7 @@ buttons = [
     [
         InlineKeyboardButton(
             text=f"Add {rem} To Your Group",
-            url=f"https://telegram.dog/{@Rem_spbot}?startgroup=true",
+            url=f"https://telegram.dog/{Rem_spbot}?startgroup=true",
         )
     ],
     [
