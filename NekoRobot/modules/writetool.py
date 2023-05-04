db3e761bb0d4917e27620d4361bb51cb22885c35
AkXbots <122038135+AkXbots@users.noreply.github.com>
2023-05-04 10:01:58 +0530

    Update writetool.py

--- NekoRobot/modules/writetool.py
+++ NekoRobot/modules/writetool.py
@@ -3,7 +3,7 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Upda
 from telegram.ext import CallbackContext
 from telegram.ext.dispatcher import run_async
 
-from FallenRobot import rem x manage, @Rem_spbot, dispatcher
+from FallenRobot import BOT_NAME, BOT_USERNAME, dispatcher
 from FallenRobot.modules.disable import DisableAbleCommandHandler
 
 
